dfd6ee2e408e3e81ad91b2fb2abb587909db9f0f
Hess-y <hessy683@gmail.com>
2022-10-19 02:02:15 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,7 +8,7 @@ void jack_bauer(void)
 	int i, j;
 
 
-	for (i = 0; i < 24; i++)
+	for (i = 0; i < 24; i++i)
 	{
 		for (j = 0; j < 60; j++)
 		{
@@ -17,10 +17,21 @@ void jack_bauer(void)
 				_putchar('0');
 				_putchar(- + '0');
 			}
+			else if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			if (j < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(j + '0');
+			}
 			else if (j >= 10)
 			{
 				_putchar(':');
-				_putchar((j / 10) +'0');
+				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
 			_putchar('\n');
