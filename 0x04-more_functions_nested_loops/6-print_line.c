0982795ffa45e91513d79393c6c39d1f2c42c2f0
Hess-y <hessy683@gmail.com>
2022-10-24 15:03:45 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.c"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times char to be printed
+ */
+void print_line(int n)
+{
+	char a;
+
+	for (a = 0; a < n; a++)
+
+		_putchar('_');
+	_putchar('\n');
+}
