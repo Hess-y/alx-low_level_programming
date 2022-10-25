26960bd903e03a7325690ee2788d9e4aa91ac292
Hess-y <hessy683@gmail.com>
2022-10-25 14:11:04 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string input
+ */
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (x >= 0)
+	{
+		if (s[x] == '\0')
+			break;
+		x++;
+	}
+	for (x--; x >= 0; x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
+
