5133892a4f9cf930ce80b6b560be3c3c44a0cc1f
Hess-y <hessy683@gmail.com>
2022-10-25 14:18:51 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ * @s: string input
+ */
+void rev_string(char *s)
+{
+	int x = 0,a,b;
+	char *str,temp;
+
+	while (x >= 0)
+	{
+		if (s[x] == '\0')
+			break;
+		x++;
+	}
+	str = s;
+
+	for (a = 0; a < (x -1); a++)
+	{
+		for (b = a +1; b > 0; b--)
+		{
+			temp = *(str + b);
+			(str +b) = (str + (b - 1));
+			*(str + (b + (b - 1)) = temp;
+		}
+	}
+}
