d48605a5fad1062c752eb1395c9d156dc5a69192
Hess-y <hessy683@gmail.com>
2022-10-28 14:27:35 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,7 +10,7 @@ char *string_toupper(char *str)
 
 	while (*(str + count))
 	{
-		if (*(str + count) >= 'A' && *(str + count) <= 'z')
+		if (*(str + count) >= 'a' && *(str + count) <= 'z')
 			*(str + count) -= 'a' - 'A';
 		count++;
 	}
