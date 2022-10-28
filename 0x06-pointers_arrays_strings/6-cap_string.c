1baf12291b49eb4e54ba2a2785f3d06f0a7e186f
Hess-y <hessy683@gmail.com>
2022-10-28 16:00:37 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,32 @@
 include "main.h"
 /**
- * cap_string - 
+ * cap_string - capitalizing all letters in string
+ * @s: string
+ * Return: address of s
+ */
+char *cap_string(char *s)
+{
+	int x = 0, y;
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + x))
+	{
+		if (*(s + x) >= 'a' && *(s + x) <= 'z')
+		{
+			if (x == 0)
+				*(s + x) -= 'a' - 'A';
+			else
+			{
+				for (y = 0; y <= 12; y++)
+				{
+					if (a[y] == *(s + x - 1))
+						*(s + x) -= 'a' - 'A';
+				}
+			}
+		}
+		x++;
+	}
+	return (s);
+#
+#
+}
