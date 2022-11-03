169b207deef1fc40b1dbf41d4d98e07c6c5d3a2f
Hess-y <hessy683@gmail.com>
2022-11-03 13:21:05 -0700

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * is_prime_number - function that checks if an input is a prime number
+ * @n: input number
+ * @c: input parameter
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+int is_prime_number(int n)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
+}
+/**
+ * is_prime_number - detects if input is a prime number
+ * @n: input number
+ * Return: 1 if prime number, otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if (n == 0)
+		return (0);
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
+}
