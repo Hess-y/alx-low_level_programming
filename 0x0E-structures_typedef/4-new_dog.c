b10c54df9a67e3640f0b546577afb7f970e79053
Hess-y <hessy683@gmail.com>
2022-11-15 14:16:57 -0800

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * new_dog -function that creates a new dog
+ * new_dog - function that creates a new dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
