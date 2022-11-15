44bc144c1a8cecf234b51564eeb440fa1a96c91f
Hess-y <hessy683@gmail.com>
2022-11-15 12:35:44 -0800

    task 0

---
+++ 0x0D-preprocessor/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to standout
+ * @c: the character to print
+ * Return: on success 1
+ * if error, -1 is returned  and error set appropriately
+ */
+
+int _putchar.c(char c)
+{
+	return (write(, &c, 1));
+}
