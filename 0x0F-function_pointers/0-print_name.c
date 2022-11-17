91e8ffdceca1ac3de9bc05d4a4b68b4e2e190322
Hess-y <hessy683@gmail.com>
2022-11-17 13:32:52 -0800

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name -  function that prints a name.
+ * print_name - function that prints a name.
  * @name: name to print
  * @f: pointer to the printing function
  */
